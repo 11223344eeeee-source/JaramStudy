63a1c018cf1a9dbd0116fb3770f3b60c7d450cc5
박서현 <11223344eeeee@gmail.com>
2026-04-02 19:28:46 +0900

    week1

---
+++ source/repos/week1/week1.c
@@ -0,0 +1,22 @@
+#define _CRT_SECURE_NO_WARNINGS 
+#include<stdio.h>
+//int main(void) {
+//	int a, b;
+//	scanf("%d %d", &a,&b);
+//	printf("%d %d %d %d %d\n", a + b, a - b, a * b, a / b, a % b);
+//	return 0;
+//}
+
+//int main(void) {
+//	int a, b, c, d, e;
+//	scanf("%d %d %d %d %d", &a, &b, &c, &d, &e);
+//	printf("%d\n", (a*a + b*b + c*c + d*d + e*e) % 10);
+//	return 0;
+//}
+
+//int main(void) {
+//	char str;
+//	scanf("%c", &str);
+//	printf("%c", str - 32);
+//	return 0;
+//}
