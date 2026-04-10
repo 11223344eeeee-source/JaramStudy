4714b1e84dc22c35f52092dbf68604406307e040
박서현 <11223344eeeee@gmail.com>
2026-04-10 11:38:48 +0900

    printMultiplicationTable

---
+++ source/repos/printMultiplicationTable/printMultiplicationTable.c
@@ -0,0 +1,12 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+int main() {
+	for (int i = 1; i <= 9; i++) {
+		for (int j = 1; j <= 9; j++) {
+			printf("| %d * %d = %2d", i, j, i * j);
+		}
+		printf(" |\n");
+	}
+    return 0;
+}
